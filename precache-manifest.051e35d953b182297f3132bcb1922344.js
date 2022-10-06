c4f957188b7d619c768eac80282d6fa1e2529d69
Dan Thomas <104170584+RetroColossus@users.noreply.github.com>
2022-10-05 22:17:20 -0400

    deploy

---
+++ precache-manifest.051e35d953b182297f3132bcb1922344.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc0391fb95471b5d70ca",
+    "url": "/dan-thomas-portfolio/static/css/main.dcb0a9e1.chunk.css"
+  },
+  {
+    "revision": "dc0391fb95471b5d70ca",
+    "url": "/dan-thomas-portfolio/static/js/main.81bdab2e.chunk.js"
+  },
+  {
+    "revision": "c9e7feab2c91f7ae3a74",
+    "url": "/dan-thomas-portfolio/static/js/runtime~main.7395e55e.js"
+  },
+  {
+    "revision": "6c1cc20bfe20899860ba",
+    "url": "/dan-thomas-portfolio/static/js/2.6b584df9.chunk.js"
+  },
+  {
+    "revision": "7f022f52ed9dd24e1ac40c31a94584ef",
+    "url": "/dan-thomas-portfolio/static/media/DanThomas_Resume.7f022f52.pdf"
+  },
+  {
+    "revision": "8dea1e8fa92d20333de125b367f3da46",
+    "url": "/dan-thomas-portfolio/static/media/background-main.8dea1e8f.png"
+  },
+  {
+    "revision": "33c2f9bc28c29c9e2cc8b3685a6c066d",
+    "url": "/dan-thomas-portfolio/static/media/hobby-central.33c2f9bc.jpg"
+  },
+  {
+    "revision": "fee30b9d2968468a3b44a2ef73d0c46e",
+    "url": "/dan-thomas-portfolio/static/media/movie-central.fee30b9d.jpg"
+  },
+  {
+    "revision": "2034cec3064ed0327465eab45fa59dae",
+    "url": "/dan-thomas-portfolio/static/media/note-taker.2034cec3.jpg"
+  },
+  {
+    "revision": "31bdff816a4dd38952258535bfccb510",
+    "url": "/dan-thomas-portfolio/static/media/password-generator.31bdff81.jpg"
+  },
+  {
+    "revision": "7ce62a6be3df115132717c30932c7a5c",
+    "url": "/dan-thomas-portfolio/static/media/taskmaster-pro.7ce62a6b.jpg"
+  },
+  {
+    "revision": "e6c5ec79276bc85ddcb64537955d88f3",
+    "url": "/dan-thomas-portfolio/static/media/tech-blog.e6c5ec79.jpg"
+  },
+  {
+    "revision": "3fd2b9970dcb7ec8c8880ab5903a4b8d",
+    "url": "/dan-thomas-portfolio/static/media/weather-dashboard.3fd2b997.jpg"
+  },
+  {
+    "revision": "8d01d6e64f45457aa5a87e929faf0541",
+    "url": "/dan-thomas-portfolio/static/media/work-day-scheduler.8d01d6e6.jpg"
+  },
+  {
+    "revision": "26d3f1e48dc0f8b9173a9c026661e863",
+    "url": "/dan-thomas-portfolio/index.html"
+  }
+];
